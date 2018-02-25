e7d82a80a88890f042add98faaf4ebd2820e9bbe
Giancarlos Salas <giansalex@gmail.com>
2018-02-24 22:10:22 -0500

    update signed to version 5

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
  */
 
 
-use Greenter\XMLSecLibs\Sunat\SunatXmlSecAdapter;
+use Greenter\XMLSecLibs\Sunat\SignedXml;
 
 require 'vendor/autoload.php';
 
 $xmlPath = __DIR__ . '/Resources/RHE1048004835615.xml';
 
-$xmlTool = new SunatXmlSecAdapter();
+$xmlTool = new SignedXml();
 
 $res = $xmlTool->verifyXml(file_get_contents($xmlPath));
 
--- sign.php
+++ sign.php
@@ -1,12 +1,12 @@
 <?php
 
-use Greenter\XMLSecLibs\Sunat\SunatXmlSecAdapter;
+use Greenter\XMLSecLibs\Sunat\SignedXml;
 
 require 'vendor/autoload.php';
 
 $xmlPath = __DIR__ . '/Resources/RHE1048004835615.xml';
 
-$xmlTool = new SunatXmlSecAdapter();
+$xmlTool = new SignedXml();
 $xmlTool->setCertificateFromFile(__DIR__.'/Resources/cert.pem');
 
 $signed = $xmlTool->signXml(file_get_contents(__DIR__.'/Resources/20000000001-01-F001-123.xml'));
